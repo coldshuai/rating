529c35c559b0fb119af60ea90538060264abfc12
BaishuaiZhang <baishuai0007@163.com>
2017-04-24 14:05:48 +0800

    上传一个文件
    test

---
+++ yewu.sql
@@ -0,0 +1,103 @@
+#国兴测试
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '1062200e-e09f-4bfd-8bb3-ce6ec0bef3f3';
+
+
+#梁穗测试
+
+#deal
+    #bizdata
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'afa85a0d-f1f3-4b88-a4f8-f350d72348cf';
+
+    
+#bizdata    
+#测试01
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '53009b4d-8e7e-40b5-aa27-ae3ba82f76c6';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'ab4aa9d6-01d7-40aa-af38-8ca7a7911318';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'eadd24d5-a7a1-4f92-8713-71d0dce975c3';
+#测试账号222 
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '5ad98293-0335-4373-929e-548784b5401c';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'a9591556-efa6-47cb-bba5-b8b6c574fec0';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '427e3117-1533-41b9-98f6-d02389b7ecb8';
+#测试账户专用10
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '5bc9755b-0078-468e-9e1a-18f16f8dd111';
+#又差点w(?Д?)w
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '262c2b60-1b50-4aae-b66e-484a77a08143';
+#adasdas
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'e6d60010-5ece-4adf-a913-5dfb9f71357f';
+#测试-驳回
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'e18d80a8-c550-46a5-8d6a-0e506873f21a';
+#测试-公路管理站
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '2d704b2a-24d1-4d5a-af78-bb16a6aad88d';
+    #rate
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '3005a800-59eb-4b73-a5da-60f234052e57';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'e352b830-29a5-4df5-9447-d1a69fcbf2f4';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'd0529685-d7f8-4bc7-a11d-cf5faf4a7450';
+    #snapshot
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '44e40df8-b406-4399-9584-d56a90b10e1a';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '131c2753-72d1-4dc5-9a58-b529879347ef';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '4dc8210c-77c9-462a-bead-0c09e6272f56';
+#测试—天下会
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '491e039d-e5e7-4141-9916-b1f0402216d9';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '9a3fb00d-94aa-4c11-8c32-f3f79af474b9';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '483b2651-61fe-4186-b89f-7b23d8937d9e';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '6e7cef34-9d0a-4a4b-aa42-c4a42c8dfeae';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '736a2abb-e76e-4916-846b-d47c7aee04df';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'a1d81646-4fbd-485b-970d-2c3baf1521c1';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '6a89dce6-be2c-432f-ac89-f1a1ff921443';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'e1440582-5f62-4fe8-89bf-517349938abd';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'b0e4290c-436b-4faf-a08e-fdbdc7bbd67e';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '00b6d2c0-8a0b-47af-8fdd-d0ed77acfce8';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'c912f05d-1047-4a34-9ad2-70763d305b50';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'c9211676-3a8b-4408-bdb3-265949437008';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'cf02009b-34b8-4d8d-84b0-782c0eb8c867';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '1bebeda7-4655-4b3e-b776-c2c7fb37cab2';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '2edd47b0-8349-42d2-aeb9-2385bcd1a709';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'dc63bdae-1349-4c29-b52d-e61dc8293847';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '7e447b71-95b5-4014-bb3b-ad98bb9ff122';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'be44db49-9bb7-4afc-86bc-8c8d69638e98';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '04f26e45-425e-49e5-b4ea-26ea67b600af';
+#测试邦峰科技
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '2b6c8341-7c59-4790-b9e0-c094b785b606';
+    #rate
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '9cf58f7d-8699-43a7-92f3-4fc0056f868e';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'e4013e76-5d49-476b-8445-4da50f4a4ea3';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '6b641327-6c42-43e4-800e-b9cef5461891';
+    #snapshot
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '4cd85081-911e-4cb7-a2e8-502738d07a1e';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'f77fe19a-bfaa-4f0e-a885-b1f064fbf270';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'ca2a5da2-5449-49ea-ace6-3a46b1c12044';
+#二期测试
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '45dd5cbc-5262-4e25-856b-9e0a4f6fb2be';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '5700c83c-6364-46f4-881c-cdb29d3b9e85';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '82e0201a-7bd1-49eb-bb3a-c1e286536077';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '3e448536-3a85-4069-b631-305900106741';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '26a03489-30c4-4ecd-868a-371b99fdc229';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '0d429412-53af-4e24-966e-691e28dbbab0';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'fd9cf059-ce93-41fa-b8d9-b90737f27b77';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'b54c9714-cae6-4110-a314-162d6f63e387';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'db701c67-6714-4d82-9693-c60fb954905e';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '46eaa65f-bfab-4ed0-a77e-0fa6239bf558';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '5cc4ca91-590f-465f-bf27-4c580b892618';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '8d62b061-5048-4a86-9ea6-81badee7657c';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'fe7c5131-f9e0-4538-a285-a552d4915ad0';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '44b2ac98-a18e-4a35-8d52-292235397bde';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '8cf13339-00a9-4910-adfd-13831e4638ab';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '6be31414-b95c-4f6b-b9aa-ffe680ff42ea';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'a0e92caa-f2d4-4a84-8677-ee8064614aae';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '6e946fd8-808f-4abd-a204-59616003851e';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'd9f7391a-b93b-4deb-a787-772b09ef58c9';
+#河北筑宾
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '2c059c53-7eb4-4a21-b2a7-ff204c5180e4';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '605360b2-2199-4389-bf42-938d9890101f';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '0e177593-590b-408c-af48-53da7cfb9a35';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'a49719d3-ba3a-4988-8fac-78e9b6f5638f';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'eb7c4c78-8b44-4180-99f1-a252dc9a9726';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'a943e124-21c2-4efa-b4aa-aea383e7aa54';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = 'a45ee348-f3a8-486b-a930-8808d2ba0f87';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '519c138c-9b98-4ce8-b994-6951603759d8';
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID = '4c07ca03-5f18-4e70-8481-e2d1551b40e3';
+#湖北三新文化传媒有限公司44
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID='2ae19762-fad8-479f-a42a-0a22261f391d';
+#珠海亿源建设工程有限公司 
+UPDATE bizdata SET OperationType = 'D' WHERE BIzID='ec5aab7d-b41b-4f13-baff-ade561ade55e';
+
